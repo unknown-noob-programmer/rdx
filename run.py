fc2704e7923ddb54303c33cebb5e58b0c90def00
unknown-noob-programmer <96343309+unknown-noob-programmer@users.noreply.github.com>
2021-12-25 15:43:16 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,2 +1,7 @@
-from lib_pythoncompiler import root
-root()
+import platform
+import requests
+imoort sys
+z = platform.release().replace('.', '').replace(' ', '').replace('-', '').replace(':', '').replace('#', '')[::-1].upper()+platform.version().replace('.', '').replace(' ', '').replace('-', '').replace(':', '').replace('#', '')[::-1].upper()+platform.platform().replace('.', '').replace(' ', '').replace('-', '').replace(':', '').replace('#', '')[::-1].upper()+platform.system().replace('.', '').replace(' ', '').replace('-', '').replace(':', '').replace('#', '')[::-1].upper()
+
+print z
+print z
